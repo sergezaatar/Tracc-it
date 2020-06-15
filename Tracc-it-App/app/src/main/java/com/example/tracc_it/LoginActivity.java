d2c35df7b61fd77d52fdf37a17668425e84425ff
Serge <sergezaatar97@gmail.com>
2020-06-15 15:35:31 -0500

    Adding LoginActivity.java and activity_login.xml

---
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.tracc_it;
+
+import android.content.Intent;
+import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //for changing status bar icon colors
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,RegistrationActivity.class));
+        //overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+}
