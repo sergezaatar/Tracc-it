d84212415861cbe0bda764da12a06b5bba2aa32a
driko-development <edrik.aguilera@mavs.uta.edu>
2020-06-15 15:40:08 -0500

    Added login method

--- Tracc-it-App/app/src/main/java/com/example/tracc_it/LoginActivity.java
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/LoginActivity.java
@@ -2,14 +2,31 @@ package com.example.tracc_it;
 
 import android.content.Intent;
 import android.os.Build;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    F I R E    B A S E    V A R I A B L E S
+    /**/    private FirebaseAuth mAuth;
+    private String TAG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +41,31 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(new Intent(this,RegistrationActivity.class));
         //overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
 
+    }
+
+    private void signIn(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            // Go to the next activity??
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
