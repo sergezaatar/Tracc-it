da5d7d3d86d3b9a903d3402b9e2eb81060d3ead8
driko-development <edrik.aguilera@mavs.uta.edu>
2020-06-15 10:38:45 -0500

    Firebase and Database methods

--- Tracc-it-App/app/src/main/java/com/example/tracc_it/RegistrationActivity.java
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/RegistrationActivity.java
@@ -2,15 +2,35 @@ package com.example.tracc_it;
 
 import android.content.Intent;
 import android.os.Build;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
         import android.view.Window;
         import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore database;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +50,55 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void onLoginClick(View view){
-        startActivity(new Intent(this,RegistrationActivity2.class));
 
+        //createAccount();
     }
 
+    private void createAccount(String email, String password, final String name, final String phone)
+    {
+        mAuth.createUserWithEmailAndPassword( email, password )
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if ( !task.isSuccessful() )
+                            Toast.makeText(RegistrationActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
 
+                        else
+                        {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            if ( user != null )
+                            {
+                                // First add the name to Firebase
+                                UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name).build();
+
+                                user.updateProfile(profileChangeRequest);
+
+                                // Add the phone to Firestore
+                                database = FirebaseFirestore.getInstance();
+                                Map<String, String> userInfo = new HashMap<>();
+                                userInfo.put("phone",phone);
+                                database.collection("users").document(mAuth.getCurrentUser().getEmail())
+                                        .set(userInfo)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error writing document", e);
+                                            }
+                                        });
+
+                                startActivity(new Intent(RegistrationActivity.this,RegistrationActivity2.class));
+                            }
+                        }
+                    }
+                });
+    }
 
 }
--- Tracc-it-App/app/src/main/java/com/example/tracc_it/RegistrationActivity2.java
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/RegistrationActivity2.java
@@ -2,15 +2,57 @@ package com.example.tracc_it;
 
 import android.content.Intent;
 import android.os.Build;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class RegistrationActivity2 extends AppCompatActivity {
+public class RegistrationActivity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    F I R E    B A S E    V A R I A B L E S
+    /**/    private FirebaseAuth mAuth;
+    /**/    private FirebaseFirestore database;
 
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    I N P U T    V A R I A B L E S
+    /**/    private Spinner spinnerGender;
+    /**/
+    /**/
+    /**/
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    U S E R    I N F O R M A T I O N
+    /**/    private int age;
+    /**/    private int[] height;
+    /**/    private int weight;
+    /**/    private String mdName;
+    /**/    private String gender;
+
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +60,14 @@ public class RegistrationActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         changeStatusBarColor();
 
+        spinnerGender =  findViewById(R.id.spinnerGender);
+
+
+        ArrayAdapter<CharSequence> adapterGender = ArrayAdapter.createFromResource(this,
+                R.array.gender_array, android.R.layout.simple_spinner_dropdown_item);
+        adapterGender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter(adapterGender);
+        spinnerGender.setOnItemSelectedListener(this);
 
     }
     private void changeStatusBarColor() {
@@ -29,11 +79,52 @@ public class RegistrationActivity2 extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        gender = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        gender = adapterView.getItemAtPosition(0).toString();
+    }
+
 //    public void onLoginClick(View view){
 //        startActivity(new Intent(this,LoginActivity.class));
 //        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
 //
 //    }
+
+    private void uploadToDatabase()
+    {
+        database = FirebaseFirestore.getInstance();
+
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("age", age);
+        userInfo.put("gender", gender);
+        userInfo.put("height", height);
+        userInfo.put("weight", weight);
+        userInfo.put("pcp", mdName);
+        //userInfo.put("checkup", );
+        //userInfo.put("lastAppt", )
+
+        database.collection("users").document(mAuth.getCurrentUser().getEmail())
+                .set(userInfo, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+
 
 
 
