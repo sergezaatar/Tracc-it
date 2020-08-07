7bd4b81f27fa7feae39a05a461233cf7974ce921
ulloajennifer89 <ulloajennifer89@yahoo.com>
2020-08-07 09:56:45 -0500

    Profile started

--- Tracc-it-App/app/src/main/java/com/example/tracc_it/HomeFragment.java
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/HomeFragment.java
@@ -4,14 +4,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class HomeFragment extends Fragment {
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    F I R E    B A S E    V A R I A B L E S
+    /**/    private FirebaseAuth mAuth;
+    /**/    private FirebaseFirestore database;
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////
+    ///*/    U S E R    I N F O R M A T I O N
+    /**/    TextView phoneNumber, email, age, height, weight,mdName, mdPhone, mdEmail;
+    /**/    Button callButton, emailButton;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        phoneNumber = view.findViewById(R.id.phoneNumber);
+        email = view.findViewById(R.id.email);
+        age = view.findViewById(R.id.age);
+        height = view.findViewById(R.id.height);
+        weight = view.findViewById(R.id.weight);
+        mdName = view.findViewById(R.id.mdName);
+        mdPhone = view.findViewById(R.id.mdPhone);
+        mdEmail = view.findViewById(R.id.mdEmail);
+        callButton = view.findViewById(R.id.callButton);
+        emailButton = view.findViewById(R.id.emailButton);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "Calling!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "Emailing!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
 }
