1dcbf673a937aae3e142af0e0b4c71cda1bf5d2f
driko-development <edrik.aguilera@mavs.uta.edu>
2020-08-08 22:23:59 -0500

    Home page updates

--- Tracc-it-App/app/src/main/java/com/example/tracc_it/HomeFragment.java
+++ Tracc-it-App/app/src/main/java/com/example/tracc_it/HomeFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +15,22 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import static android.Manifest.permission.CALL_PHONE;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class HomeFragment extends Fragment {
 
@@ -30,11 +39,12 @@ public class HomeFragment extends Fragment {
     ///*/    F I R E    B A S E    V A R I A B L E S
     /**/    private FirebaseAuth mAuth;
     /**/    private FirebaseFirestore database;
+    DocumentReference userReference;
 
     /////////////////////////////////////////////////////////
     ////////////////////////////////////
     ///*/    U S E R    I N F O R M A T I O N
-    /**/    TextView phoneNumber, email, age, height, weight,mdName, mdPhone, mdEmail;
+    /**/    TextView phoneNumber, email, age, height, weight,mdName, mdPhone, mdEmail, name;
     /**/    Button callButton, emailButton;
 
 
@@ -43,8 +53,13 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
     public void onViewCreated(View view, Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        // Set up firebase stuff
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
+
+        // Set up view variables
+        name = view.findViewById(R.id.welcomeText);
         phoneNumber = view.findViewById(R.id.phoneNumber);
         email = view.findViewById(R.id.email);
         age = view.findViewById(R.id.age);
@@ -58,6 +73,7 @@ public class HomeFragment extends Fragment {
         String Subject = "Appointment Check-In";
         String message = "I am confirming my appointment";
 
+        updateProfileInfo();
 
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +93,56 @@ public class HomeFragment extends Fragment {
             }
         });
     }
+
+    private void updateProfileInfo() {
+
+        // Update users name
+        updateFirebaseInfo();
+
+        // Update datbase info
+        userReference = database.collection("users").document(mAuth.getCurrentUser().getEmail());
+
+        userReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful())
+                {
+                    DocumentSnapshot doc = task.getResult();
+                    phoneNumber.setText(phoneNumber.getText().toString().concat(": " +
+                            doc.getString("phone").replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3")));
+                    age.setText(age.getText().toString().concat(": " + doc.get("age").toString()));
+                    height.setText(height.getText().toString().concat(": " + doc.getString("height")));
+                    weight.setText(weight.getText().toString().concat(": " + doc.get("weight").toString()));
+
+                    updateDoctorInfo();
+                }
+            }
+        });
+    }
+
+    private void updateFirebaseInfo() {
+        name.setText(name.getText().toString().split(" ")[0]  // Gets "Welcome,"
+                .concat(" " + mAuth.getCurrentUser().getDisplayName() + "!")); // adds user's name + "!"
+        email.setText(email.getText().toString().concat(": " + mAuth.getCurrentUser().getEmail()));
+    }
+
+    private void updateDoctorInfo() {
+        userReference.collection("doctor").whereEqualTo("isPCP", true)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                // This loop should only run once
+                for (QueryDocumentSnapshot doctor : task.getResult())
+                {
+                    mdName.setText(doctor.getId());
+                    mdEmail.setText(doctor.getString("email"));
+                    mdPhone.setText(doctor.getString("phone").replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));
+                }
+            }
+        });
+
+    }
+
     public void onCall(View view) {
 //        Intent i = new Intent(Intent.ACTION_CALL);
 //        i.setData(Uri.parse("tel: 222-222-2222"));
